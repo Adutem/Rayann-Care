16f3f49d64a93cba443a5e9f74dfd1a503947360
mella <oyegbemilelatifat@gmail.com>
2025-06-28 16:21:26 +0100

    this

--- router/contactroute.js
+++ router/contactroute.js
@@ -1,4 +1,4 @@
-import { createContact } from "../controller/Contactcontroller.js";
+import { createContact } from "../controller/contactcontroller.js"
 import express from "express";
 
 const router = express.Router()
