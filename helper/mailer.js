159127a20aa7fcf4f18072448e20982fbcde7d23
mella <oyegbemilelatifat@gmail.com>
2025-07-04 13:38:43 +0100

    ola

--- helper/mailer.js
+++ helper/mailer.js
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  // host: process.env.MAIL_HOST,
-  // port: parseInt(process.env.MAIL_PORT),
-  // secure: process.env.MAIL_SECURE === 'true', 
-  service:"gmail",
+  host: process.env.MAIL_HOST,
+  port: parseInt(process.env.MAIL_PORT),
+  secure: process.env.MAIL_SECURE === 'true', 
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
